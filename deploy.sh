e4fce64d288bf91941dd9e4d11db74c850d55a48
saegeullee <lemix777@naver.com>
2020-08-15 03:21:06 +0900

    Implemented travis deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 echo "$SSH_PASSWORD" | ssh root@49.247.207.38 --password-stdin
-pwd
+docker ps --filter name=client* -aq | xargs docker stop
+docker run --name client-$SHA -p 80:3000 lemix777/capstone-client:$SHA
