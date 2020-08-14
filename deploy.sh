28b3d88aaf2640eaa679fa864edcf336b9f6c34d
saegeullee <lemix777@naver.com>
2020-08-15 04:06:55 +0900

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-echo "$SSH_PASSWORD" | ssh root@49.247.207.38 --password-stdin
+apt-get install sshpass
+sshpass -p "$SSH_PASSWORD" ssh root@49.247.207.38
 docker ps --filter name=client* -aq | xargs docker stop
 docker run --name client-$SHA -p 80:3000 -d lemix777/capstone-client:$SHA
 exit
