7a57123af475b5129b423098873ff99ba99530f6
saegeullee <lemix777@naver.com>
2020-08-15 03:28:45 +0900

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 echo "$SSH_PASSWORD" | ssh root@49.247.207.38 --password-stdin
 docker ps --filter name=client* -aq | xargs docker stop
 docker run --name client-$SHA -p 80:3000 -d lemix777/capstone-client:$SHA
+exit
