37d2d59f97efcdb93bca479aa274fbab8f0e9ef8
saegeullee <lemix777@naver.com>
2020-08-20 02:48:29 +0900

    fixed generateUploadUrl

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,23 +1,15 @@
 import 'source-map-support/register';
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
 
-import { getImageUploadUrl } from '../../businessLogic/diary';
+import DiaryBusinessLogicInstanceGetter from '../../businessLogic/diary';
 import { UploadUrlResponse } from '../../responses/UploadUrlResponse';
+import { Responses } from '../../common/API_Responses';
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log('current event: ', event);
 
-  const urlResponse: UploadUrlResponse = getImageUploadUrl();
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    body: JSON.stringify({
-      item: urlResponse
-    })
-  };
+  const urlResponse: UploadUrlResponse = DiaryBusinessLogicInstanceGetter().getImageUploadUrl();
+  return Responses._200({ item: urlResponse });
 };
