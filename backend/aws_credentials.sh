8e9c822f0872d7d67e77d81412760a98c4c272f6
saegeullee <lemix777@naver.com>
2020-08-22 03:38:48 +0900

    fixed travis

--- backend/aws_credentials.sh
+++ backend/aws_credentials.sh
@@ -4,5 +4,9 @@ cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id=${AWS_ACCESS_KEY}
 aws_secret_access_key=${AWS_SECRET_KEY}
+EOL
+
+cat > ~/.aws/config << EOL
+[default]
 region=ap-northeast-2
 EOL
